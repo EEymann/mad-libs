b9d4e314e8f9dc50168517b5770fff3334318948
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2019-06-12 10:29:28 -0700

    refactor js with looping.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+    
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
